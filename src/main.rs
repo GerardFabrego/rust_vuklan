6a587740d9e95dec82d8a482acc5613680371dcb
Gerard <gerardfabrego@gmail.com>
2022-10-23 19:41:17 +0200

    Multiply all numbers from a buffer by 12

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 use vulkano::{
-    instance::{Instance, InstanceCreateInfo},
-    device::physical::PhysicalDevice,
-    device::{Device, DeviceCreateInfo, Features, QueueCreateInfo},
     buffer::{BufferUsage, CpuAccessibleBuffer},
-    command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferInfo},
-    sync:: {self, GpuFuture}
+    command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage},
+    descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet},
+    device::{Device, DeviceCreateInfo, QueueCreateInfo},
+    instance::{Instance, InstanceCreateInfo},
+    pipeline::{ComputePipeline, Pipeline, PipelineBindPoint},
+    sync::{self, GpuFuture},
+    VulkanLibrary,
 };
 
-
 fn main() {
-    let instance = Instance::new(InstanceCreateInfo::default()).expect("failed to create instance");
-    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
 
-    let queue_family = physical.queue_families()
-    .find(|&q| q.supports_graphics())
-    .expect("couldn't find a graphical queue family");
+    let instance =
+        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+    let physical = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
+
+    let queue_family_index = physical
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_, q)| q.queue_flags.graphics)
+        .expect("couldn't find a graphical queue family") as u32;
 
     let (device, mut queues) = Device::new(
         physical,
         DeviceCreateInfo {
             // here we pass the desired queue families that we want to use
-            queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
             ..Default::default()
         },
     )
@@ -28,40 +42,89 @@ fn main() {
 
     let queue = queues.next().unwrap();
 
-
     // Create buffer from iterator
-    let source_content: Vec<i32> = (0..64).collect();
-    let source = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, source_content)
-        .expect("failed to create buffer");
+    let data_iter = 0..65536;
+    let data_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage {
+            storage_buffer: true,
+            ..Default::default()
+        },
+        false,
+        data_iter,
+    )
+    .expect("failed to create buffer");
 
-    let destination_content: Vec<i32> = (0..64).map(|_| 0).collect();
-    let destination = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, destination_content)
-        .expect("failed to create buffer");
+    mod cs {
+        vulkano_shaders::shader! {
+            ty: "compute",
+            src: "
+                #version 450
 
+                layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+                
+                layout(set = 0, binding = 0) buffer Data {
+                    uint data[];
+                } buf;
+                
+                void main() {
+                    uint idx = gl_GlobalInvocationID.x;
+                    buf.data[idx] *= 12;
+                }"
+        }
+    }
 
-    // Build command buffer
+    let shader = cs::load(device.clone()).expect("failed to create shader module");
+
+    let compute_pipeline = ComputePipeline::new(
+        device.clone(),
+        shader.entry_point("main").unwrap(),
+        &(),
+        None,
+        |_| {},
+    )
+    .expect("failed to create compute pipeline");
+
+    let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+    let set = PersistentDescriptorSet::new(
+        layout.clone(),
+        [WriteDescriptorSet::buffer(0, data_buffer.clone())], // 0 is the binding
+    )
+    .unwrap();
+
     let mut builder = AutoCommandBufferBuilder::primary(
         device.clone(),
-        queue.family(),
+        queue.queue_family_index(),
         CommandBufferUsage::OneTimeSubmit,
     )
     .unwrap();
 
-    builder.copy_buffer(CopyBufferInfo::buffers(source.clone(), destination.clone())).unwrap();
+    builder
+        .bind_pipeline_compute(compute_pipeline.clone())
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            0, // 0 is the index of our set
+            set,
+        )
+        .dispatch([1024, 1, 1])
+        .unwrap();
 
     let command_buffer = builder.build().unwrap();
 
-
-    // Send command buffer to GPU and execute it
     let future = sync::now(device.clone())
-    .then_execute(queue.clone(), command_buffer)
-    .unwrap()
-    .then_signal_fence_and_flush() // same as signal fence, and then flush
-    .unwrap();
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
 
     future.wait(None).unwrap();
 
-    let src_content = source.read().unwrap();
-    let destination_content = destination.read().unwrap();
-    assert_eq!(&*src_content, &*destination_content);
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        assert_eq!(*val, n as u32 * 12);
+    }
+
+    println!("Everything succeeded!");
+
 }
